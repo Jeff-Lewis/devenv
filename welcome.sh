da908d744108bcde5da5349e5b1e8132f4e85d45
Aric <social@bastionweb.io>
2018-02-25 14:47:12 -0700

    Update welcome.sh

--- welcome.sh
+++ welcome.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
 
+if [ ! -f /home/dev/credentials/id_rsa ]; then
+    echo """
+        /home/dev/credentials/id_rsa not found
+        If you create a RSA key here we'll auto add it's identity on ssh
+    """
+else
+    eval $(ssh-agent -s)
+    ssh-add ~/credentials/id_rsa
+fi
+
+if [ ! -f /home/dev/credentials/aws ]; then
+    echo """
+        /home/dev/credentials/aws not found
+        If you create a file with your aws credentials here we'll move it to the appropriate location
+    """
+else
+    mkdir ~/.aws
+    cp ~/credentials/aws ~/.aws/credentials
+fi
+
 echo """
    ___           ____        
   / _ \___ _  __/ __/__ _  __
